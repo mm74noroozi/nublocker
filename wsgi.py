574dc13b87d59d38cf27781d8bc887efaffccaf3
mohammad <mm.74noroozi@gmail.com>
2019-05-23 00:04:31 -0700

    what the fuck is going on...?

--- wsgi.py
+++ wsgi.py
@@ -1,37 +1,9 @@
-from flask import Flask, session, redirect, url_for, escape, request
-import requests as rq
-from bs4 import BeautifulSoup as bs
+from flask import Flask
 application = Flask(__name__)
 
-# Set the secret key to some random bytes. Keep this really secret!
-application.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-main="<form method='post'>url<input type=text name=search><input type=submit value=GO></form>"
+@application.route("/")
+def hello():
+    return "Hello World!"
 
-@application.route('/',methods=['GET', 'POST'])
-def login():
-	if request.method == 'POST':
-		str=request.form['search']
-		if str[:4] != 'http':
-			str='https://'+str
-		r=rq.get(str)
-		response=bs(r.text,'html.parser')
-		for img in response.select("[src]"):
-			if img['src'][:4]== 'http':
-				img['src']='/files?q='+img['src']
-			else:
-				img['src']='/files?q='+r.url+img['src'][1:]	
-		for link in response.select("[href]"):
-			if link['href'][:4]== 'http':
-				link['href']='/files?q='+link['href']
-			else:
-				link['href']='/files?q='+r.url+link['href'][1:]
-		return main+response.prettify()
-	return main
-
-@application.route('/files',methods=['GET','POST'])
-def files():
-	return rq.get(request.args['q']).content, 200, {'Content-Type': 'application'}
-	
-if __name__ == '__main__':
-    application.run(debug=True)
-
+if __name__ == "__main__":
+    application.run()
